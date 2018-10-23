dc8c542637b22d634a21a38850ad4b19d216ed44
Tomek Wójcik <vooydzig@gmail.com>
2018-10-23 21:47:51 +0200

    lesson 26 - dataframes pt. 2

---
+++ jobs/data_frame_2.py
@@ -0,0 +1,40 @@
+import os
+
+from pyspark.sql import SparkSession
+
+from config import DATA_DIR
+
+INPUT = os.path.join(DATA_DIR, 'appl_stock.csv')
+
+
+def setup():
+    spark = SparkSession.builder.appName("BasicOps").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    return spark
+
+
+def filter_sql(df):
+    return df.filter("Close < 500")
+
+
+def filter_python(df):
+    return df.filter(df['Close'] < 500)
+
+
+def filter_multiple(df):
+    return df.filter((df['Open'] > 200) & (df['Close'] < 200))
+
+
+def get_by_low_value(df, val):
+    return df.filter(df['Low'] == val).collect()
+
+
+spark = setup()
+df = spark.read.csv(INPUT, inferSchema=True, header=True)
+
+filter_sql(df).select(['Open', 'Close']).show()
+filter_python(df).select(['Open', 'Close']).show()
+filter_multiple(df).select(['Open', 'Close']).show()
+result = get_by_low_value(df, 197.16)
+row = result[0]
+print(row.asDict())
